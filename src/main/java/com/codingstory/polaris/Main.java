58608dbfc1fce5472a9e22f0a2293e7d4460812f
Chao Shi <stepinto@live.com>
2012-08-19 00:42:46 +0800

    Simple search webui

--- src/main/java/com/codingstory/polaris/Main.java
+++ src/main/java/com/codingstory/polaris/Main.java
@@ -3,6 +3,7 @@ package com.codingstory.polaris;
 import com.codingstory.polaris.indexing.DirectoryIndexer;
 import com.codingstory.polaris.parser.JavaTokenExtractor;
 import com.codingstory.polaris.parser.Token;
+import com.codingstory.polaris.search.SimpleWebServer;
 import com.google.common.collect.ImmutableList;
 import org.apache.commons.io.IOUtils;
 
@@ -13,18 +14,26 @@ import java.io.InputStream;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        if (args.length == 0) {
-            printHelp();
-        }
-        String command = args[0];
-        List<String> commandArgs = ImmutableList.copyOf(args).subList(1, args.length);
-        if (command.equalsIgnoreCase("parse")) {
-            runParse(commandArgs);
-        } else if (command.equalsIgnoreCase("index")) {
-            runIndex(commandArgs);
-        } else {
-            printHelp();
+    public static void main(String[] args) {
+        try {
+            if (args.length == 0) {
+                printHelp();
+                System.exit(0);
+            }
+            String command = args[0];
+            List<String> commandArgs = ImmutableList.copyOf(args).subList(1, args.length);
+            if (command.equalsIgnoreCase("parse")) {
+                runParse(commandArgs);
+            } else if (command.equalsIgnoreCase("index")) {
+                runIndex(commandArgs);
+            } else if (command.equalsIgnoreCase("searchui")) {
+                runSearchUI(commandArgs);
+            } else {
+                printHelp();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
         }
     }
 
@@ -51,9 +60,18 @@ public class Main {
         DirectoryIndexer.main(args.toArray(new String[args.size()]));
     }
 
+    private static void runSearchUI(List<String> args) throws InterruptedException, IOException {
+        SimpleWebServer server = new SimpleWebServer();
+        int port = 8080;
+        server.setPort(port);
+        System.out.println("Server: http://localhost:" + port);
+        server.run();
+    }
+
     private static void printHelp() {
         System.out.println("Usage:");
         System.out.println("  polaris parse files...");
+        System.out.println("  polaris searchui");
         System.out.println();
     }
 }
